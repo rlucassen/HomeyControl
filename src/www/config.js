cbfaccf2c74adadb7146daa1e8e1c13f01650c22
Robin Lucassen <robin@lucassen.me>
2017-02-06 22:59:10 +0100

    initial

---
+++ src/www/config.js
@@ -0,0 +1,104 @@
+﻿var bearerToken = '';
+var homeyIp = '192.168.178.12';
+var houseMapping = {
+  width: 16160,
+  height: 6950,
+  general: {
+    devices: {
+      '9be96690-edb7-462b-a6f3-a160baf7cda7': {
+        type: 'presence'
+      },
+      '7b527f53-208c-4e77-b5bd-13b0c6012af4': {
+        type: 'thermostat'
+      }
+    }
+  },
+  doors: {
+    '8c2e1c02-220c-45a2-80a5-42505e22be64': {
+      width: 1000,
+      height: 70,
+      x: 11980,
+      y: 2835,
+    }
+  },
+  chambers: {
+    woonkamer: {
+      width: 7981,
+      height: 6950,
+      x: 0,
+      y: 0,
+      background: 'content/img/woonkamer.jpg',
+      devices: {
+        '91649b22-e65c-4dbc-8adf-e11bb80c88f3': {
+          type: 'lamp',
+          x: 2700,
+          y: 2000
+        },
+        '8080aa09-c175-4363-9e06-140146278c23': {
+          type: 'dimlamp',
+          x: 5700,
+          y: 5400
+        },
+        'b9c96972-defb-4dcc-8ad7-e42b2272b7c5': {
+          type: 'lamp',
+          x: 5700,
+          y: 1800
+        },
+        'b9c509a8-cac3-4feb-974b-35579e723795': {
+          type: 'dimlamp',
+          x: 3200,
+          y: 5600
+        },
+        '13f5499b-e497-45a8-b1c6-d430e2e9f0c7': {
+          type: 'lamp',
+          x: 2000,
+          y: 6500
+        },
+        '7d3b7261-1d42-411b-a5f7-2f92aa2d20fd': {
+          type: 'lamp',
+          x: 350,
+          y: 300
+        },
+      }
+    },
+    bijkeuken: {
+      width: 2999,
+      height: 2585,
+      x: 8051,
+      y: 4365,
+    },
+    gang: {
+      width: 8109,
+      height: 1390,
+      x: 8051,
+      y: 2905,
+      background: 'content/img/gang.jpg',
+      motion: '5371b0c3-5470-47fb-ab14-f734eaac32b0',
+      devices: {
+        'b5308d09-aee5-469a-9028-683f5429dcfd': {
+          type: 'dimlamp',
+          x: 4200,
+          y: 700
+        },
+      }
+    },
+    badkamer: {
+      width: 2969,
+      height: 2805,
+      x: 8081,
+      y: 0
+    },
+    slaapkamer: {
+      width: 4970,
+      height: 2835,
+      x: 11180,
+      y: 0
+    },
+    werkkamer: {
+      width: 5040,
+      height: 2585,
+      x: 11120,
+      y: 4365
+    }
+  }
+}
